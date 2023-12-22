d8af172260c2a017df7bcd02de954865f29135f0
Serafina R <serafina-reif@gmx.de>
2023-12-22 19:14:25 +0100

    normalisation row issue fixed

--- bin/normalization.py
+++ bin/normalization.py
@@ -4,9 +4,12 @@
 import anndata as ad
 import scanpy as sc
 from scipy.sparse import issparse
+from scipy.sparse import csr_matrix
 import rpy2.robjects as ro
 import anndata2ri
 import sys
+import os
+os.environ['NUMBA_CACHE_DIR'] = '/tmp/numba_cache'
 
 #R packages
 ro.r('library(Seurat)')
@@ -17,6 +20,11 @@ adata_path = sys.argv[1]
 output_file = sys.argv[2]
 
 adata = sc.read_h5ad(adata_path)
+
+gene_counts = adata.var_names.value_counts()
+duplicates = gene_counts[gene_counts > 1]
+if not duplicates.empty:
+    adata.var_names_make_unique()
 
 sc.pp.filter_genes(adata, min_cells=5)
     
@@ -29,6 +37,8 @@ for key in adata.layers:
         if not adata.layers[key].has_sorted_indices:
             adata.layers[key].sort_indices()
 
+adata.obs_names_make_unique()
+
 ro.globalenv['adata'] = adata
 
 ro.r('seurat_obj = as.Seurat(adata, counts="X", data = NULL)')
@@ -36,6 +46,7 @@ ro.r('seurat_obj = as.Seurat(adata, counts="X", data = NULL)')
 ro.r('res <- SCTransform(object=seurat_obj, assay = "originalexp", return.only.var.genes = FALSE, do.correct.umi = FALSE)')
 
 norm_x = ro.r('res@assays$SCT@scale.data').T
+norm_x = csr_matrix(norm_x)
 
 adata_transformed = ad.AnnData(X=norm_x, obs=adata.obs)
 # BASICALLY number of features differ: 6660 (original) vs 6645 (normalized)
